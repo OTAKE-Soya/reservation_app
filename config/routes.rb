a003d4f23ab2899c5a5b4016c7c4d9d0281f2df9
OTAKE-Soya <oyassun@gmail.com>
2021-03-25 11:11:44 +0000

    top作った、root設定した、deviseのcontrollerとview生成した、マイグレートした

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
     passwords:     'users/passwords',
     registrations: 'users/registrations'
   }
-  root to: 'home#index'
+  root to: 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
